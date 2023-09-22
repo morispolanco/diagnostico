e8174e8651a4cee23215dce7e6de2d8a1955cac7
morispolanco <morispolanco@gmail.com>
2023-09-22 01:51:51 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ def assistant():
 
     openai.api_key = openai_api_key
 
-    prompt = "Analiza la competencia lingüística del autor en un texto dado, evaluando gramática, vocabulario, uso de tiempos verbales, coherencia, cohesión, ortografía y estilo de escritura. Proporciona una evaluación detallada, resaltando fortalezas y áreas de mejora. Utiliza ejemplos del texto para respaldar tus observaciones. Asegúrate de presentar un análisis equilibrado y completo."
+    prompt = "Realiza un diagnóstico de la competencia lingüística del autor del texto evaluando gramática, ortografía, puntuación, vocabulario y estructura de oraciones. Examina su capacidad para comunicar ideas y uso de estilos y tonos según el contexto. Usa ejemplos del texto para respaldar tus hallazgos y, si encuentras áreas mejorable, sugiere recomendaciones apropiadas para mejorar su competencia."
     
     if prompt:
         st.session_state.messages.append({"role": "user", "content": prompt})
