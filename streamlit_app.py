3f1c0b4f4c5d75b4fcf8234039ad2529f067c6c9
morispolanco <morispolanco@gmail.com>
2023-09-22 01:41:52 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,8 @@ def assistant():
 
     openai.api_key = openai_api_key
 
-    prompt = "Tu tarea es hacer una evaluación diagnóstica del siguiente texto. Proporciona un análisis detallado y crítico del texto, identificando sus puntos fuertes y áreas de mejora. Explica tus observaciones de manera clara y concisa, brindando ejemplos relevantes para respaldar tus puntos de vista. Tu evaluación debe ser objetiva y constructiva, ofreciendo sugerencias útiles para mejorar el texto. También puede ser útil proporcionar recomendaciones para la estructura, organización y estilo del texto, si es necesario. "
+    prompt = "Realiza una evaluación diagnóstica del texto proporcionado, considerando tanto aspectos gramaticales como de estilo. Proporciona una respuesta detallada y precisa que identifique y analice los errores gramaticales y sugerir mejoras para el estilo. En tu respuesta, asegúrate de explicar claramente los errores gramaticales encontrados, proporcionando ejemplos específicos y ofreciendo una revisión precisa de cada uno. Además, sugiere cambios y mejoras para mejorar el estilo del texto, teniendo en cuenta la estructura de las oraciones, la elección de las palabras y la coherencia general. Por favor, asegúrate de ofrecer explicaciones claras y consejos útiles para el autor del texto, con el objetivo de ayudarles a mejorar su habilidad gramatical y estilo de escritura."
+    
     if prompt:
         st.session_state.messages.append({"role": "user", "content": prompt})
 
