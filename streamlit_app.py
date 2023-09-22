ed1be66b16d9dcf71ab1e3baab1809be6f3e636d
morispolanco <morispolanco@gmail.com>
2023-09-22 01:43:37 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ def assistant():
 
     openai.api_key = openai_api_key
 
-    prompt = "Realiza una evaluación diagnóstica del texto proporcionado, considerando tanto aspectos gramaticales como de estilo. Proporciona una respuesta detallada y precisa que identifique y analice los errores gramaticales y sugerir mejoras para el estilo. En tu respuesta, asegúrate de explicar claramente los errores gramaticales encontrados, proporcionando ejemplos específicos y ofreciendo una revisión precisa de cada uno. Además, sugiere cambios y mejoras para mejorar el estilo del texto, teniendo en cuenta la estructura de las oraciones, la elección de las palabras y la coherencia general. Por favor, asegúrate de ofrecer explicaciones claras y consejos útiles para el autor del texto, con el objetivo de ayudarles a mejorar su habilidad gramatical y estilo de escritura."
+    prompt = "Realiza una evaluación diagnóstica del texto proporcionado, considerando tanto aspectos gramaticales como de estilo. Proporciona una respuesta detallada y precisa que identifique y analice los errores gramaticales y sugerir mejoras para el estilo. En tu respuesta, asegúrate de explicar claramente los errores gramaticales encontrados, proporcionando ejemplos específicos y ofreciendo una revisión precisa de cada uno. Además, sugiere cambios y mejoras para mejorar el estilo del texto, teniendo en cuenta la estructura de las oraciones, la elección de las palabras y la coherencia general."
     
     if prompt:
         st.session_state.messages.append({"role": "user", "content": prompt})
